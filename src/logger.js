f306b0fbeb63bde56a6aecd3a5714aa39b8da001
Ricardo Gonzalez <rickgc@gmail.com>
2024-06-11 17:09:38 +0100

    ✨ Use winston instead of console.log()

---
+++ src/logger.js
@@ -0,0 +1,8 @@
+import winston from 'winston';
+
+const { createLogger, format, transports } = winston;
+export const logger = createLogger({
+  level: process.env.LOG_LEVEL || 'info',
+  format: format.cli(),
+  transports: [new transports.Console()],
+});
