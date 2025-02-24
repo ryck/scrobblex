7b4f35ff9d0c17abcccdd16314d05fb736ff85ee
Ricardo Gonzalez <rickgc@gmail.com>
2025-02-24 11:54:30 +0000

    💥 Remove tailwind CDN dependency

--- src/index.js
+++ src/index.js
@@ -33,6 +33,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(errorHandler)
+app.use(express.static('static'))
 app.use('/favicon.ico', express.static('favicon.ico'));
 app.set('view engine', 'ejs');
 
